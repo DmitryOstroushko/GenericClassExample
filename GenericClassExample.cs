43d923cefb60efb16ee07063f26f73f6b0ea394f
Dmitry Ostroushko <dmitrylianne@gmail.com>
2024-04-23 21:14:12 -0400

    Add files via upload

---
+++ GenericClassExample.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Linq;
+
+namespace GenericClassExample
+{
+    public class GenericClassExample<T> where T : class
+    {
+        private ICollection<T> _collection;
+
+        public void Add(T item)
+        {
+            _collection.Add(item);
+        }
+
+        public T Get(int number) 
+        {
+            if (number < _collection.Count)
+            {
+                return _collection.ElementAt(number);
+            }
+            return null;
+        }
+
+        public ICollection<T> GetOrderedCollection() {
+            return _collection.OrderByDescending(r => r.GetHashCode());
+        }
+    }
+}
